94e8c08016d4488b6c75e7aa2b19a3163a91b25b
Adeepa_Bimsara <adeepabimsara6@gmail.com>
2026-08-09 21:38:25 +0530

    update

--- src/app/components/about-section/about.tsx
+++ src/app/components/about-section/about.tsx
@@ -24,7 +24,7 @@ export const About = () => {
             src={personalData.profile}
             width={280}
             height={280}
-            alt="Abu Said"
+            alt="Adeepa Bimsara"
             className="rounded-lg transition-all duration-1000 grayscale hover:grayscale-0 hover:scale-110 cursor-pointer"
             style={{ width: 'auto', height: 'auto' }}
           />
