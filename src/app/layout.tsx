eef1fd3d4d41fef6a1dd956ce9d8b510e32b31af
Adeepa_Bimsara <adeepabimsara6@gmail.com>
2026-08-09 10:42:49 +0530

    implement footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 
 
 const geistSans = Geist({
@@ -40,6 +41,8 @@ export default function RootLayout({
             {children}
           </div>
         </main>
+
+        <Footer />
 
       </body>
     </html>
